f9302c2971c46b183d19b6fba3ccc34d21d011a5
Andrew Qiao <andrewqiao2004@gmail.com>
2024-01-07 13:16:58 -0500

    projects section responsiveness

--- src/components/elements/ProjectCard.tsx
+++ src/components/elements/ProjectCard.tsx
@@ -1,8 +1,7 @@
-import type { StaticImageData } from 'next/image';
-import GradientBorder from '../ui/GradientBorder';
-import GradientText from '../typography/GradientText';
-import { Badge } from '../ui/Badge';
-import Image from 'next/image';
+import Image, { type StaticImageData } from 'next/image';
+import { Badge } from '@/components/ui/Badge';
+import GradientBorder from '@/components/ui/GradientBorder';
+import GradientText from '@/components/typography/GradientText';
 
 type ProjectCardProps = {
   id: string;
@@ -18,19 +17,21 @@ export default function ProjectCard({
   mockup,
 }: ProjectCardProps) {
   return (
-    <GradientBorder borderRadius="rounded-[21px] md:even:translate-y-9">
-      <div className="relative aspect-[35/22] overflow-hidden rounded-5 bg-background">
+    <GradientBorder borderRadius="rounded-[21px] xs:rounded-[25px] md:rounded-[21px] md:even:translate-y-9 xl:rounded-[25px]">
+      <div className="relative aspect-[35/22] overflow-hidden rounded-5 bg-background xs:rounded-6 md:rounded-5 xl:rounded-6">
         <Image
           src={mockup}
           alt={`${title} mockup`}
           className="absolute right-[15%] top-1/2 w-[42%] -translate-y-1/2"
         />
         <div className="absolute inset-0 bg-light-card-bottom-gradient dark:bg-dark-card-bottom-gradient" />
-        <h3 className="absolute bottom-6 left-6 font-display text-6 font-bold">
+        <h3 className="absolute bottom-6 left-6 font-display text-6 font-bold xs:bottom-8 xs:left-8 xs:text-8 md:bottom-6 md:left-6 md:text-6 xl:bottom-8 xl:left-8 xl:text-8">
           <GradientText>{title}</GradientText>
         </h3>
-        <GradientBorder borderRadius="rounded-full absolute right-4 top-4">
-          <Badge className="font-display">{type}</Badge>
+        <GradientBorder borderRadius="rounded-full absolute right-4 top-4 xs:top-5 xs:right-5 md:top-4 md:right-4 xl:top-5 xl:right-5">
+          <Badge className="font-display xs:px-3.5 xs:py-2 xs:text-3.5 md:px-3 md:py-1.5 md:text-3 xl:px-3.5 xl:py-2 xl:text-3.5">
+            {type}
+          </Badge>
         </GradientBorder>
       </div>
     </GradientBorder>
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,7 +2,7 @@ import GradientText from '../typography/GradientText';
 
 export default function Hero() {
   return (
-    <section className="container mb-9 xs:mb-12 lg:mb-16">
+    <section className="container mb-9 md:mb-12 xl:mb-16">
       <h1 className="max-w-[350px] font-display text-7 font-bold xs:max-w-[460px] xs:text-9 xs:leading-[56px] sm:max-w-[550px] lg:max-w-[742px] lg:text-12 lg:leading-[76px]">
         I&apos;m <GradientText>Andrew Qiao</GradientText>. I build what the
         world needs <GradientText>tomorrow</GradientText>, today.
